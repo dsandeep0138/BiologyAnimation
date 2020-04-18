803eee6ae287d5ecc9ee9fb0dd3af5c2c9f44d30
srichalla66 <srichalla66@tamu.edu>
2020-04-17 21:58:07 -0500

    updated slide 46

--- slide46/DD_slide46.js
+++ slide46/DD_slide46.js
@@ -1,34 +1,69 @@
 $(document).ready(function () {
-    var correct =0;
-    $(".answer").draggable();
+    var answered=[]
+    var magicString = "position: relative; width: 100px; inset: 0px auto auto 0px; height: 15px"
+    var moveBack = function(id){
+        $('#'+id).attr("style", magicString);
+    }
+    $(".answer").draggable({
+        revert: "invalid",
+        revertDuration: 0,
+        start:function(ev, ui){
+            //console.log($(this).text());
+        }
+    });
     $(".question").droppable({
-        refreshPositions: true,
-        over: function(event, ui) {
-            $(this).css('background', 'orange');
-        },
         out: function(event, ui) {
-            $(this).css('background', 'cyan');
+			if($.inArray($(this).text(), answered) < 0) {
+                if($(this).attr("ans") == ui.draggable.attr('id')) {
+                    console.log("plz")
+                    $(this).css('background', 'cyan');
+                    $(this).attr("ans", "");
+                }
+            }
         },
         drop: function(event, ui) {
-            $(this).css('background', 'orange');
-            var $el = ui.draggable;
-            var pid= $el.attr("id");
-            $('#'+pid).draggable("disable");
-            $(this).attr("id",pid);
+			if($.inArray($(this).text(), answered) < 0) {
+                if ($(this).attr("ans") != ui.draggable.attr('id')) {
+                    moveBack($(this).attr("ans"));
+                }
+                $(this).css('background', 'orange');
+                $(this).attr("ans",ui.draggable.attr('id'));
+                //console.log(ui.draggable);
+            }
+            else {
+                moveBack(ui.draggable.attr('id'));
+            }
+        }
+    });
+    $(".question").dblclick(function(){
+        console.log($(this));
+        if($.inArray($(this).attr("ans"), answered) < 0) {
+            moveBack($(this).attr("ans"));
+            $(this).css('background', 'cyan');
+            $(this).attr("ans", 0);
         }
     });
     $( "#submit" ).click(function() {
+		var correct =0;
         $('.question').each(function() {
-            console.log($(this).attr("id"));
-            if($(this).attr("id") == $(this).text()){
+            if($(this).attr("ans") == $(this).text()){
                 correct=correct+1;
+			    if($.inArray($(this).text(), answered) < 0)
+				    answered.push($(this).text())
                 $(this).css('background', 'green');
+				$('#'+$(this).attr("ans")).draggable('disable');
             }else{
-                $(this).css('background', 'red');
-                $('#hint').text('Hint: Go back and look at Cell Diagram');
-                $('#hint').css('font', '12px');
+                //$('#hint').text('Hint: Go back and study the diagram in the section Organisms');
+                //$('#hint').css('font', '12px');
+                if($(this).attr("ans")) {
+                    $(this).css('background', 'red');
+                    moveBack($(this).attr("ans"));
+                }
             }
         });
-       alert( "You got " + correct +" answers correct.\n");
+        if (correct == 11)
+            alert( "You got " + correct +" answers correct.\n");
+        else
+            alert( "You got " + correct +" answers correct.\nHint: Go back and study the diagram in the section Organisms");
     });
 });
