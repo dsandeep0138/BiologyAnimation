a50a2e51f3d883287e0ac8ee23f9cd3a03736969
Devi Sandeep Endluri <dsandeep97@tamu.edu>
2020-10-20 00:20:24 -0500

    Move OrderedImages to other repo

--- OrderedImages/js/index.js
+++
@@ -1,7 +0,0 @@
-var iframeResize = require('./iframeResizer')
-
-exports.iframeResize = iframeResize
-exports.iframeResizer = iframeResize // Backwards compatability
-exports.iframeResizerContentWindow = require('./iframeResizer.contentWindow')
-
-var sorting = require('./sorting')
