aac714e6fa70a2277d9155fd76a688ffafa600ab
Devi Sandeep Endluri <dsandeep97@tamu.edu>
2020-10-29 15:13:39 -0500

    Add randomization, and showing correct responses

--- tilematching/js/tilematching.js
+++ tilematching/js/tilematching.js
@@ -1,6 +1,26 @@
 window.onload = function() {
     var grid = document.getElementById("puzzle-grid");
     var numChildren = grid.childElementCount;
+
+    // randomize the list
+    for (i = numChildren; i >= 0; i--) {
+        grid.appendChild(grid.children[Math.random() * i | 0]);
+    }
+
+    var set1 = document.getElementsByClassName("set1");
+    var set2 = document.getElementsByClassName("set2");
+    var set3 = document.getElementsByClassName("set3");
+    var set4 = document.getElementsByClassName("set4");
+
+    set11 = set1[0].cloneNode(true); 
+    set12 = set1[1].cloneNode(true); 
+    set21 = set2[0].cloneNode(true); 
+    set22 = set2[1].cloneNode(true); 
+    set31 = set3[0].cloneNode(true); 
+    set32 = set3[1].cloneNode(true); 
+    set41 = set4[0].cloneNode(true); 
+    set42 = set4[1].cloneNode(true); 
+
     var outputText = document.getElementById("output-text");
     var clickedTiles = [];
 
@@ -31,6 +51,28 @@ window.onload = function() {
                         tiles[i].innerHTML = "";
 		        tiles[i].id = "removed";
                         tiles[i].removeEventListener("click", matchTile);
+		    }
+
+		    var count = 0;
+                    for (i = 0; i < numChildren; i++) {
+                        if (grid.children[i].id == "removed") {
+                            count += 1;
+                        }
+		    }
+
+                    if (count == 8) {
+			grid.innerHTML = "";
+
+			grid.appendChild(set11);
+			grid.appendChild(set21);
+			grid.appendChild(set31);
+			grid.appendChild(set41);
+			grid.appendChild(set12);
+			grid.appendChild(set22);
+			grid.appendChild(set32);
+			grid.appendChild(set42);
+
+                        outputText.innerHTML = "All tiles are matched!";
 		    }
 		});
 	    } else {
