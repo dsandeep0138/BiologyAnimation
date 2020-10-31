5799513d0afb4358bc8e03a47302ffaf8d33ae89
Devi Sandeep Endluri <dsandeep97@tamu.edu>
2020-10-30 23:46:08 -0500

    Added reset and counter for moves.

--- 8-puzzle/js/8-puzzle.js
+++ 8-puzzle/js/8-puzzle.js
@@ -5,6 +5,48 @@ window.onload = function() {
     var numChildren = grid.childElementCount;
     var directions = [[-1, 0], [0, 1], [0, -1], [1, 0]];
     var outputText = document.getElementById("output-text");
+    var moves = document.getElementById("moves");
+
+    var container = document.getElementById("container");
+    var gridClone = grid.cloneNode(true);
+
+    var counter = 0;
+    moves.value = counter;
+
+    function initialSetup() {
+        var emptyCellRow = Math.floor(emptyCellIndex / 3);
+        var emptyCellColumn = emptyCellIndex % 3;
+
+        for (i = 0; i < directions.length; i++) {
+            if (emptyCellRow + directions[i][0] >= 0 &&
+                emptyCellRow + directions[i][0] < 3) {
+                if (emptyCellColumn + directions[i][1] >= 0 &&
+                    emptyCellColumn + directions[i][1] < 3) {
+                    index = convertGridToIndex(emptyCellRow + directions[i][0],
+			                       emptyCellColumn + directions[i][1]);
+                    grid.children[index].addEventListener("click", swap);
+                }
+            }
+        }
+    }
+
+    document.getElementById("reset").onclick = function resetGrid() {
+        container.innerHTML = "";
+        container.appendChild(gridClone);
+        outputText.innerHTML = "";
+
+        grid = document.getElementById("puzzle-grid");
+        emptycell = document.getElementById("empty");
+        emptyCellIndex = Array.prototype.indexOf.call(grid.children, emptycell);
+        numChildren = grid.childElementCount;
+
+        container = document.getElementById("container");
+        gridClone = grid.cloneNode(true);
+
+        counter = 0;
+        moves.value = counter;
+        initialSetup();
+    }
 
     function checkAnswer() {
         for (i = 0; i < numChildren - 1; i++) {
@@ -17,6 +59,9 @@ window.onload = function() {
     }
 
     function swap() {
+        counter += 1;
+        moves.value = counter;
+
         for (i = 0; i < numChildren; i++) {
             grid.children[i].removeEventListener("click", swap);
         }
@@ -74,18 +119,5 @@ window.onload = function() {
         return;
     }
 
-    var emptyCellRow = Math.floor(emptyCellIndex / 3);
-    var emptyCellColumn = emptyCellIndex % 3;
-
-    for (i = 0; i < directions.length; i++) {
-        if (emptyCellRow + directions[i][0] >= 0 &&
-            emptyCellRow + directions[i][0] < 3) {
-            if (emptyCellColumn + directions[i][1] >= 0 &&
-                emptyCellColumn + directions[i][1] < 3) {
-                index = convertGridToIndex(emptyCellRow + directions[i][0],
-			                   emptyCellColumn + directions[i][1]);
-                grid.children[index].addEventListener("click", swap);
-            }
-        }
-    }
+    initialSetup();
 }
