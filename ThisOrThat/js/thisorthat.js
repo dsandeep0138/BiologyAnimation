8615db4c2e12c66d2e5184d958f5962f9f2af48a
Devi Sandeep Endluri <dsandeep97@tamu.edu>
2020-11-11 10:41:23 -0600

    Modify look and feel of This or That animation.

--- ThisOrThat/js/thisorthat.js
+++ ThisOrThat/js/thisorthat.js
@@ -10,17 +10,20 @@ window.onload = function() {
     };
 
     function q1Function(btn_id) {
+        console.log(btn_id);
         if (btn_id == 1) {
             document.getElementById("q1option1").disabled = true;
             document.getElementById("q1option2").disabled = true;
 
             outputText.innerHTML = "That's right!";
+            outputText.style.color = "blue";
             var x = document.getElementById("question1");
             if (x.style.display === "none") {
                 x.style.display = "block";
             } 
         } else {
             outputText.innerHTML = "That's wrong! Try again. \nHint: Go back and take a look at types of cells";
+            outputText.style.color = "red";
         }
     }
 
@@ -33,11 +36,13 @@ window.onload = function() {
     };
 
     function q2Function(btn_id) {
+        console.log(btn_id);
         if (btn_id == 1) {
             document.getElementById("q2option1").disabled = true;
             document.getElementById("q2option2").disabled = true;
             outputText.innerHTML = "That's right! Now try this next one";
             var y = document.getElementById("question2");
+            outputText.style.color = "blue";
             if (y.style.display == "none") {
                 y.style.display = "block";
             } else {
@@ -45,18 +50,26 @@ window.onload = function() {
             }
         } else {
             outputText.innerHTML = "That's wrong! Try again. \nHint: Go back and take a look at types of cells";
+            outputText.style.color = "red";
 	}
     }
 
-    document.getElementById("q3option1").onclick = function() {q3Function(1)};
-    document.getElementById("q3option2").onclick = function() {q3Function(2)};
+    document.getElementById("q3option1").onclick = function() {
+        q3Function(1)
+    };
+
+    document.getElementById("q3option2").onclick = function() {
+        q3Function(2)
+    };
 
     function q3Function(btn_id) {
+        console.log(btn_id);
         if (btn_id == 1) {
             document.getElementById("q3option1").disabled = true;
             document.getElementById("q3option2").disabled = true;
             outputText.innerHTML = "That's right! Now try this next one";
             var x = document.getElementById("question3");
+            outputText.style.color = "blue";
             if (x.style.display == "none") {
                 x.style.display = "block";
             } else {
@@ -64,6 +77,7 @@ window.onload = function() {
             }
         } else {
             outputText.innerHTML = "That's wrong! Try again. \nHint: Go back and take a look at types of cells";
+            outputText.style.color = "red";
         }
     }
 }
