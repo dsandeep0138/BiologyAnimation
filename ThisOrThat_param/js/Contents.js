937b2788865cbdb6d25eee13a055c7282ff5e43e
Himani Deshpande <hdeshpande11@tamu.edu>
2020-11-14 20:30:43 +0000

    parameterized this or that

---
+++ ThisOrThat_param/js/Contents.js
@@ -0,0 +1,27 @@
+
+    var questions = [
+   
+        {
+       "question" : "It often has appendages.",
+        "options" : ["Prokaryotic Cell","Eukaryotic Cell"],
+        answer : "Prokaryotic Cell"
+            
+        },
+        
+        {
+        "question" : "Animal cell and Plant cell are types of this cell.",
+        "options" : ["Prokaryotic Cell","Eukaryotic Cell"],
+        "answer" : "Eukaryotic Cell"
+            
+        },
+        
+      {
+        "question" : "It has no membrane-bound organelles.",
+        "options" : ["Prokaryotic Cell","Eukaryotic Cell"],
+        "answer" : "Prokaryotic Cell"
+            
+        }
+   
+   
+];
+
