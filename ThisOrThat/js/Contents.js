cb075648926a09ad5d71544df9bf8bdbdba8d59a
Devi Sandeep Endluri <dsandeep97@tamu.edu>
2020-11-15 00:14:17 -0600

    clean up This or That code.

--- ThisOrThat/js/Contents.js
+++ ThisOrThat/js/Contents.js
@@ -1,35 +1,57 @@
+var questions = [
+{
+    "question" : "It often has appendages.",
+    "options" : ["Prokaryotic Cell", "Eukaryotic Cell"],
+    "answer" : "Prokaryotic Cell",
+    "hint" : "That's Incorrect! Try again or Revisit Cell Types!",
+    "topic" : "Cell Types"
+},
 
-    var questions = [
-   
-        {
-       "question" : "It often has appendages.",
-        "options" : ["Prokaryotic Cell","Eukaryotic Cell"],
-        "answer" : "Prokaryotic Cell",
-        "hint" : "That's Incorrect! Try again or Revisit Cell Types!",
-        "type" : "Cell Types"
-            
-        },
-        
-        {
-        "question" : "Animal cell and Plant cell are types of this cell.",
-        "options" : ["Prokaryotic Cell","Eukaryotic Cell"],
-        "answer" : "Eukaryotic Cell",
-        "hint" : "That's Incorrect! Try again or Revisit Cell Types!",
-        "type" : "Cell Types"
+{
+    "question" : "Animal cell and Plant cell are types of this cell.",
+    "options" : ["Prokaryotic Cell", "Eukaryotic Cell"],
+    "answer" : "Eukaryotic Cell",
+    "hint" : "That's Incorrect! Try again or Revisit Cell Types!",
+    "topic" : "Cell Types"
+},
 
-            
-        },
-        
-      {
-        "question" : "It has no membrane-bound organelles.",
-        "options" : ["Prokaryotic Cell","Eukaryotic Cell"],
-        "answer" : "Prokaryotic Cell",
-        "hint" : "That's Incorrect! Try again or Revisit Cell Types!",
-        "type" : "Cell Types"
+{
+    "question" : "It has no membrane-bound organelles.",
+    "options" : ["Prokaryotic Cell", "Eukaryotic Cell"],
+    "answer" : "Prokaryotic Cell",
+    "hint" : "That's Incorrect! Try again or Revisit Cell Types!",
+    "topic" : "Cell Types"
+},
 
-            
-        }
-   
-   
+{
+    "question" : "Non-membrane bound structures which make proteins. They can either be free floating in the cytosol or they can be bound to the endoplasmic reticulum.",
+    "options" : ["Ribosomes", "Mitochondria"],
+    "answer" : "Ribosomes",
+    "hint" : "That's incorrect! Try again or Revisit Cell Structures!",
+    "topic" : "Cell Structures"
+},
+
+{
+    "question" : "The site of respiration which is the conversion of chemical energy stored in food (glucose) plus oxygen into ATP (cell's energy source), CO2, and water.",
+    "options" : ["Ribosomes", "Mitochondria"],
+    "answer" : "Mitochondria",
+    "hint" : "That's incorrect! Try again or Revisit Cell Structures!",
+    "topic" : "Cell Structures"
+},
+
+{
+    "question" : "A stacked, flattened membrane which processes and modifies proteins.",
+    "options" : ["Endoplasmic Reticulum", "Golgi Body"],
+    "answer" : "Golgi Body",
+    "hint" : "That's incorrect! Try again or Revisit Cell Structures!",
+    "topic" : "Cell Structures"
+},
+
+{
+    "question" : "A series of folded membranes which process proteins and sends them to the cell membrane, exports them from the cell, or sends them to other cell compartments.",
+    "options" : ["Endoplasmic Reticulum", "Golgi Body"],
+    "answer" : "Endoplasmic Reticulum",
+    "hint" : "That's incorrect! Try again or Revisit Cell Structures!",
+    "topic" : "Cell Structures"
+}
 ];
-
--- ThisOrThat/js/thisorthat.js
+++ ThisOrThat/js/thisorthat.js
@@ -1,68 +1,46 @@
 window.onload = function() {
-
-var q_num=0;
-
+    var q_num = 0;
 
     var outputText = document.getElementById("output-text");
     var h2 = document.getElementById("question_no");
-        var p = document.getElementById("question_body");
+    var p = document.getElementById("question_body");
     var button1 = document.getElementById("option1");
     var button2 = document.getElementById("option2");
 
-
-    
-genQuestion(q_num)
+    genQuestion(q_num);
 
     document.getElementById("option1").onclick = function() {
-       
         qFunction(document.getElementById("option1").textContent)
-        
     };
 
     document.getElementById("option2").onclick = function() {
         qFunction(document.getElementById("option2").textContent)
     };
 
-
-
-    function genQuestion(q_num)
-    {
-        h2.innerHTML = "Question " + (q_num+1) + " of " + questions.length +" [Topic: "+ questions[q_num]["type"] + "]" ;
-               p.innerHTML = questions[q_num]["question"];
+    function genQuestion(q_num) {
+        h2.innerHTML = "Question " + (q_num+1) + " of " + questions.length +" [Topic: "+ questions[q_num]["topic"] + "]" ;
+        p.innerHTML = questions[q_num]["question"];
         button1.innerHTML = questions[q_num]["options"][0]
         button2.innerHTML = questions[q_num]["options"][1]
-       
     }
 
-     function qFunction(value)
-     {
-         console.log(value);
-         console.log(questions[q_num]["answer"]);
-         if(value.localeCompare(questions[q_num]["answer"]) ==0)
-         {
-             q_num += 1;
-             outputText.innerHTML = "That's right!";
-             if(q_num >= questions.length)
-             {
+    function qFunction(value)
+    {
+        if (value.localeCompare(questions[q_num]["answer"]) == 0)
+        {
+            q_num += 1;
+            outputText.innerHTML = "That's right!";
+
+            if (q_num >= questions.length) {
                 document.getElementById("option1").disabled = true;
                 document.getElementById("option2").disabled = true;
                 h2.disabled = true;
                 p.disabled = true;
-             }
-             else
-             {
-	    	    genQuestion(q_num);
-		       
-             }
-         }
-         else
-         {
-             outputText.innerHTML = questions[q_num]["hint"] ;
-         }
-         
-      
-     }
+            } else {
+                genQuestion(q_num);
+            }
+        } else {
+            outputText.innerHTML = questions[q_num]["hint"];
+        }
+    }
 }
-
-
-    
