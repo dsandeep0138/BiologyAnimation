589bfb918eb336bc3c2668f32b92119fd86794fa
Devi Sandeep Endluri <dsandeep97@tamu.edu>
2020-11-18 17:01:17 -0600

    keep only cell types module.

--- ThisOrThat/js/contents.js
+++ ThisOrThat/js/contents.js
@@ -21,37 +21,5 @@ var questions = [
     "answer" : "Prokaryotic Cell",
     "hint" : "That's Incorrect! Revisit Cell Types!",
     "topic" : "Cell Types"
-},
-
-{
-    "question" : "Non-membrane bound structures which make proteins. They can either be free floating in the cytosol or they can be bound to the endoplasmic reticulum.",
-    "options" : ["Ribosomes", "Mitochondria"],
-    "answer" : "Ribosomes",
-    "hint" : "That's incorrect! Revisit Cell Structures!",
-    "topic" : "Cell Structures"
-},
-
-{
-    "question" : "The site of respiration which is the conversion of chemical energy stored in food (glucose) plus oxygen into ATP (cell's energy source), CO2, and water.",
-    "options" : ["Ribosomes", "Mitochondria"],
-    "answer" : "Mitochondria",
-    "hint" : "That's incorrect! Revisit Cell Structures!",
-    "topic" : "Cell Structures"
-},
-
-{
-    "question" : "A stacked, flattened membrane which processes and modifies proteins.",
-    "options" : ["Endoplasmic Reticulum", "Golgi Body"],
-    "answer" : "Golgi Body",
-    "hint" : "That's incorrect! Revisit Cell Structures!",
-    "topic" : "Cell Structures"
-},
-
-{
-    "question" : "A series of folded membranes which process proteins and sends them to the cell membrane, exports them from the cell, or sends them to other cell compartments.",
-    "options" : ["Endoplasmic Reticulum", "Golgi Body"],
-    "answer" : "Endoplasmic Reticulum",
-    "hint" : "That's incorrect! Revisit Cell Structures!",
-    "topic" : "Cell Structures"
 }
 ];
